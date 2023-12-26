6a78b10fc77b404977e70210cdd3a5684180bd69
nandanpi <nandanrp09@gmail.com>
2023-12-27 00:57:54 +0530

    feat: Add Hero to _page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+"use client";
 import Image from "next/image";
+import Hero from "@/components/hero/page";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <Image
-          src="/assets/ech_full_logo.png"
-          alt="ech_logo"
-          width={300}
-          height={300}
-        />
-        <h1 className="md:text-7xl text-3xl font-antonio">
-          Ethereum Cat Herders V3
-        </h1>
-      </div>
+      <Hero />
     </>
   );
 }
