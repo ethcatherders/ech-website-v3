87d5edb5080e5330405db09df7f4482b7cfe472d
Nandan Pai <nandanrp09@gmail.com>
2024-05-02 12:03:32 +0530

    Network Upgrade Accordion

--- src/constants/networkUpgrades.ts
+++ src/constants/networkUpgrades.ts
@@ -1,4 +1,6 @@
 import { NetworkUpgrades } from "@/types/page";
+import { exec } from "child_process";
+import { version } from "os";
 
 export const networkUpgrades: NetworkUpgrades[] = [
     {
@@ -373,4 +375,395 @@ export const networkUpgrades: NetworkUpgrades[] = [
         date: " 03/14/2016"
     }
   ]
+
+
+export const accordionData = [
+    {
+        upgradeName: "Dencun",
+        links: [
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-1153",
+                linkTitle: "EIP-1153: Transient storage opcodes"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-4788",
+                linkTitle: "EIP-4788: Beacon block root in the EVM"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-4844",
+                linkTitle: "EIP-4844: Shard Blob Transactions"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-5656",
+                linkTitle: "EIP-5656: MCOPY - Memory copying instructions"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-6780",
+                linkTitle: "EIP-6780: SELFDESTRUCT only in same transaction"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-7044",
+                linkTitle: "EIP-7044: Perpetually Valid Signed Voluntary Exits"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-7045",
+                linkTitle: "EIP-7045: Increase Max Attestation Inclusion Slot"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-7514",
+                linkTitle: "EIP-7514: Add Max Epoch Churn Limit"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-7516",
+                linkTitle: "EIP-7516: BLOBBASEFEE opcode"
+            },
+            {
+                link: "https://blog.ethereum.org/2024/02/27/dencun-mainnet-announcement",
+                linkTitle: "Dencun Mainnet Announcement"
+            },
+        ]
+    },
+    {
+        upgradeName: "Shapella",
+        links: [
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-3651",
+                linkTitle: "EIP-3651: Warm COINBASE"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-3855",
+                linkTitle: "EIP-3855: PUSH0 instruction"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-3860",
+                linkTitle: "EIP-3860: Limit and meter initcode"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-4895",
+                linkTitle: "EIP-4895 : Beacon chain push withdrawals as operations"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-6049",
+                linkTitle: "EIP-6049: Deprecate SELFDESTRUCT"
+            },
+            {
+                link: "https://blog.ethereum.org/2023/03/28/shapella-mainnet-announcement",
+                linkTitle: "Shapella Mainnet Announcement"
+            },
+        ]
+    },
+    {
+        upgradeName: "The Merge",
+        links: [
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-4399",
+                linkTitle: "EIP-4399 : Supplant DIFFICULTY opcode with PREVRANDAO"
+            },
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-3675",
+                linkTitle: "EIP-3675 : Upgrade consensus to Proof-of-Stake"
+            },
+            {
+                link: "https://blog.ethereum.org/2022/08/24/mainnet-merge-announcement",
+                linkTitle: "Mainnet Merge Announcement"
+            },
+        ]
+    },
+    {
+        upgradeName: "Gray Glacier",
+        links: [
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-5133",
+                linkTitle: "EIP-5133: Delaying Difficulty Bomb to mid-September 2022"
+            },
+            {
+                link: "https://blog.ethereum.org/2022/06/16/gray-glacier-announcement/",
+                linkTitle: "Gray Glacier Upgrade Announcement"
+            },
+        ]
+    },
+    {
+        upgradeName: "Arrow Glacier",
+        links: [
+            {
+                link: "https://eips.ethereum.org/EIPS/eip-4345",
+                linkTitle: "EIP-4345: Difficulty Bomb Delay to June 2022"
+            },
+            {
+                link: "https://medium.com/ethereum-cat-herders/ethereum-arrow-glacier-upgrade-e8d20fa4c002",
+                linkTitle: "Ethereum Arrow Glacier Upgrade"
+            },
+            {
+                link: "https://medium.com/ethereum-cat-herders/ethereum-arrow-glacier-upgrade-e8d20fa4c002",
+                linkTitle: "Arrow Glacier Upgrade Announcement"
+            }
+        ]
+    },
+    {
+        upgradeName: "Altair Upgrade",
+        links: [
+            {
+                link: "https://blog.ethereum.org/2021/10/05/altair-announcement/",
+                linkTitle: "Altair Mainnet Announcement"
+            },
+        ]
+    }
+]
+
+
+export const Downloads = [
+    {
+        upgradeName: "Dencun",
+        consensus: [
+            {
+                client: "Lighthouse",
+                version: "v5.0.0",
+                link: "https://github.com/sigp/lighthouse/releases/tag/v5.0.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Lodestar",
+                version: "v1.17.0",
+                link: "https://github.com/ChainSafe/lodestar/releases/tag/v1.17.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nimbus",
+                version: "v24.2.2",
+                link: "https://github.com/status-im/nimbus-eth2/releases/tag/v24.2.2",
+                linkTitle: "Release"
+            },
+            {
+                client: "Prysm",
+                version: "v5.0.2",
+                link: "https://github.com/prysmaticlabs/prysm/releases/tag/v5.0.2",
+                linkTitle: "Download"
+            },
+            {
+                client: "Teku",
+                version: "v24.2.0",
+                link: "https://github.com/Consensys/teku/releases/tag/24.2.0",
+                linkTitle: "Download"
+            },
+        ],
+        execution: [
+            {
+                client: "Besu",
+                version: "24.3.0",
+                link: "https://github.com/hyperledger/besu/releases/tag/24.3.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Erigon",
+                version: "v2.58.0",
+                link: "https://github.com/ledgerwatch/erigon/releases/tag/v2.58.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "go-ethereum (geth)",
+                version: "v1.13.12",
+                link: "https://github.com/ethereum/go-ethereum/releases/tag/v1.13.12",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nethermind",
+                version: "v1.25.4",
+                link: "https://github.com/NethermindEth/nethermind/releases/tag/1.25.4",
+                linkTitle: "Download"
+            }
+        ]
+    },
+    {
+        upgradeName: "Shapella",
+        consensus: [
+            {
+                client: "Lighthouse",
+                version: "v4.0.1",
+                link: "https://github.com/sigp/lighthouse/releases/tag/v4.0.1",
+                linkTitle: "Download"
+            },
+            {
+                client: "Lodestar",
+                version: "v1.7.0",
+                link: "https://github.com/ChainSafe/lodestar/releases/tag/v1.7.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nimbus",
+                version: "v23.3.2",
+                link: "https://github.com/status-im/nimbus-eth2/releases/tag/v23.3.2",
+                linkTitle: "Release"
+            },
+            {
+                client: "Prysm",
+                version: "v4.0.0",
+                link: "https://github.com/prysmaticlabs/prysm/releases/tag/v4.0.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Teku",
+                version: "v23.3.1",
+                link: "https://github.com/ConsenSys/teku/releases/tag/23.3.1",
+                linkTitle: "Download"
+            },
+        ],
+        execution: [
+            {
+                client: "Besu",
+                version: "v23.1.2",
+                link: "https://github.com/hyperledger/besu/releases/tag/23.1.2",
+                linkTitle: "Download"
+            },
+            {
+                client: "Erigon",
+                version: "v2.42.0",
+                link: "https://github.com/ledgerwatch/erigon/releases/tag/v2.42.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "go-ethereum (geth)",
+                version: "v1.11.5",
+                link: "https://github.com/ethereum/go-ethereum/releases/tag/v1.11.5",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nethermind",
+                version: "v1.17.3",
+                link: "https://github.com/NethermindEth/nethermind/releases/tag/1.17.3",
+                linkTitle: "Download"
+            }
+        ]
+    },
+    {
+        upgradeName: "The Merge",
+        consensus: [
+            {
+                client: "Lighthouse",
+                version: "v3.1.0",
+                link: "https://github.com/sigp/lighthouse/releases/tag/v3.1.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Lodestar",
+                version: "v1.0.0",
+                link: "https://github.com/ChainSafe/lodestar/releases/tag/v1.0.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nimbus",
+                version: "v22.9.0",
+                link: "https://github.com/status-im/nimbus-eth2/releases/tag/v22.9.0",
+                linkTitle: "Release"
+            },
+            {
+                client: "Prysm",
+                version: "v3.1.1",
+                link: "https://github.com/prysmaticlabs/prysm/releases/tag/v3.1.1",
+                linkTitle: "Download"
+            },
+            {
+                client: "Teku",
+                version: "v22.9.0",
+                link: "https://github.com/ConsenSys/teku/releases/tag/22.9.0",
+                linkTitle: "Download"
+            },
+        ],
+        execution: [
+            {
+                client: "Besu",
+                version: "v22.7.2",
+                link: "https://github.com/hyperledger/besu/releases/tag/22.7.2",
+                linkTitle: "Download"
+            },
+            {
+                client: "Erigon",
+                version: "v2022.09.01 - alpha",
+                link: "https://github.com/ledgerwatch/erigon/releases/tag/v2022.09.01",
+                linkTitle: "Release"
+            },
+            {
+                client: "go-ethereum (geth)",
+                version: "v1.10.23",
+                link: "https://github.com/ethereum/go-ethereum/releases/tag/v1.10.23",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nethermind",
+                version: "v1.14.1",
+                link: "https://github.com/NethermindEth/nethermind/releases/tag/1.14.1",
+                linkTitle: "Download"
+            }
+        ]
+    },
+    {
+        upgradeName: "Gray Glacier",
+        clientSpecs: [
+            {
+                client: "go ethereum (geth)",
+                version: "v1.10.19",
+                link: "https://github.com/ethereum/go-ethereum/releases/tag/v1.10.19",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nethermind",
+                version: "v1.13.3",
+                link: "https://github.com/NethermindEth/nethermind/releases/tag/1.13.3",
+                linkTitle: "Download"
+            },
+            {
+                client: "Erigon",
+                version: "v2022.06.03-alpha",
+                link: "https://github.com/ledgerwatch/erigon/releases/tag/v2022.06.03",
+                linkTitle: "Release"
+            },
+            {
+                client: "Besu",
+                version: "v22.4.3",
+                link: "https://github.com/hyperledger/besu/releases/tag/22.4.3",
+                linkTitle: "Download"
+            }
+        ]
+    },
+    {
+        upgradeName: "Arrow Glacier",
+        clientSpecs: [
+            {
+                client: "go ethereum (geth)",
+                version: "v1.10.12",
+                link: "https://github.com/ethereum/go-ethereum/releases/tag/v1.10.12",
+                linkTitle: "Download"
+            },
+            {
+                client: "Nethermind",
+                version: "v1.11.7",
+                link: "https://github.com/NethermindEth/nethermind/releases/tag/1.11.7",
+                linkTitle: "Download"
+            },
+            {
+                client: "Erigon",
+                version: "v2021.11.01 - alpha",
+                link: "https://github.com/ledgerwatch/erigon/releases/tag/v2021.11.01",
+                linkTitle: "Release"
+            },
+            {
+                client: "Prysm",
+                version: "v2.0.0",
+                link: "https://github.com/prysmaticlabs/prysm/releases/tag/v2.0.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "Besu",
+                version: "v21.10.0",
+                link: "https://github.com/hyperledger/besu/releases/tag/21.10.0",
+                linkTitle: "Download"
+            },
+            {
+                client: "EthereumJS VM",
+                version: "v5.6.0",
+                link: "https://github.com/ethereumjs/ethereumjs-monorepo/releases/tag/%40ethereumjs%2Fvm%405.6.0",
+                linkTitle: "Download"
+            }
+        ] 
+    }
+]
   
