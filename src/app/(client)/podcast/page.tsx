a207d965fc0fa6086d65f3beb8ded34b84a2780a
geovgy <gbh218@gmail.com>
2024-12-16 20:07:10 -0500

    Update /podcast page content and layout

--- src/app/(client)/podcast/page.tsx
+++ src/app/(client)/podcast/page.tsx
@@ -10,30 +10,45 @@ export default function Podcast() {
             <h1 className="xl:text-6xl lg:text-4xl sm:text-4xl text-3xl text-left font-roboto font-bold text-darkGray">
               EthCatHerders Podcasts
             </h1>
-            <h3 className="xl:text-2xl text-lightGray">
+            {/* <h3 className="xl:text-2xl text-lightGray">
               In depth interviews of the people building our ecosystem and
               beyond....
+            </h3> */}
+            <h3 className="text-lg xl:text-2xl text-lightGray">
+              Your go-to source for staying updated on all things Ethereum. We
+              bring you the latest insights from Ethereum Core Dev Meetings,
+              deep dives into PEEPanEIP, exciting demos of ecosystem projects,
+              and exclusive interviews with key players in the Ethereum space.
+            </h3>
+            <h3 className="text-lg xl:text-2xl text-lightGray">
+              Whether you're here to learn, stay informed, or explore the
+              vibrant Ethereum community, we’ve got you covered with engaging
+              content designed to educate and connect. Join us as we unravel the
+              ever-evolving world of Ethereum! 🐾✨
             </h3>
           </div>
 
-          <h1 className="xl:text-6xl lg:text-4xl sm:text-4xl text-3xl text-left font-roboto font-bold text-darkGray py-12">
+          {/* <h1 className="xl:text-6xl lg:text-4xl sm:text-4xl text-3xl text-left font-roboto font-bold text-darkGray py-12">
             Listen to the podcasts of ethereum-cat-herders
-          </h1>
-
-          <div className="grid grid-cols-2 md:grid-cols-3 md:gap-10 gap-5">
-            {podcastCardData.map((item: PodcastCards, index) => {
-              return <PodcastCard card={item} key={index} />;
-            })}
-          </div>
+          </h1> */}
 
           <h1 className="xl:text-6xl lg:text-4xl sm:text-4xl text-3xl text-left font-roboto font-bold text-darkGray py-12">
             Stay updated with the latest podcasts right here!!
           </h1>
+          <div className="flex flex-col-reverse lg:flex-row gap-5 justify-start">
+            <iframe
+              className="w-full h-[800px] lg:h-[650px] pb-8"
+              src="https://anchor.fm/ethereum-cat-herders"
+            ></iframe>
+            <div className="grid grid-cols-2 gap-5 sm:min-w-[500px] sm:max-h-[400px] pb-8">
+              {podcastCardData.map((item: PodcastCards, index) => {
+                return <PodcastCard card={item} key={index} />;
+              })}
+            </div>
+          </div>
 
-          <iframe
-            className="w-full h-[800px] pb-8"
-            src="https://anchor.fm/ethereum-cat-herders"
-          ></iframe>
+
+
         </div>
       </div>
     </>
